9dbd5c57478e0de34efb81f8e9faeab303ab457a
avi <avikhajuria1994@outlook.com>
2022-09-01 02:08:07 +0530

    knex changes

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 db.select('*').from('users').then(data =>{
