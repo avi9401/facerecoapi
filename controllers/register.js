0aeccc2e4bc46e70d094e99ffd3796c50c73ec11
avi <avikhajuria1994@outlook.com>
2022-09-01 02:34:51 +0530

    pushing

--- controllers/register.js
+++ controllers/register.js
@@ -5,7 +5,7 @@ const handleRegister = (req, res, db, bcrypt)=> {
 		return res.status(400).json('incorrect submission form')
 	}
 	const hash = bcrypt.hashSync(password);
-	 db.transaction(trx => {
+	    db.transaction(trx => {
 			trx.insert({
 				hash: hash,
 				email: email
@@ -14,14 +14,14 @@ const handleRegister = (req, res, db, bcrypt)=> {
 			.returning('email')
 			.then(loginEmail => {
 			   return trx('users')
-					 .returning('*')
-					 .insert({
-							email: loginEmail[0].email,
-							name: name,
-							joined: new Date()
-						})
-					  .then(user => {
-						   res.json(user[0]);
+					.returning('*')
+					.insert({
+						email: loginEmail[0].email,
+						name: name,
+						joined: new Date()
+					})
+					.then(user => {
+						res.json(user[0]);
 				    })
 
 			})
@@ -29,7 +29,7 @@ const handleRegister = (req, res, db, bcrypt)=> {
 			.catch(trx.rollback)
 		})
 		
-	.catch(err => res.status(400).json('unable to register'));
+	    .catch(err => res.status(400).json('unable to register'));
 }
 
 module.exports = {
