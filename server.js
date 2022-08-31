af26612102cef5bbfc0e5f6f8362e56676475bc2
avi <avikhajuria1994@outlook.com>
2022-08-31 07:53:08 +0530

    second commit

--- server.js
+++ server.js
@@ -29,9 +29,7 @@ app.use(cors());
 
 
 
-app.get('/', (req, res)=>{
-	res.json('success');
-})
+app.get('/', (req, res)=>{ res.send('success') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
 
